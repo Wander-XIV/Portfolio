ce7a69a059ef811da26532d8778b9d6e4d93d62d
Wander-XIV <52870778+Wander-XIV@users.noreply.github.com>
2022-10-06 17:51:27 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# :seedling: Portfolio
+
+Personal portfolio website - Final Version
+
+- Displays all the projects that I completed in my Bootcamp as well as some info about myself and how to contact me
+
+## :hammer_and_wrench: Built With 
+
+- HTML
+
+- CSS
+
+- JavaScript
+
+## :rocket: <a href="https://wander-xiv.github.io/Portfolio-Site/">Live Demo</a> 
---
+++ script.js
@@ -0,0 +1,44 @@
+let prevScrollpos = window.pageYOffset;
+window.onscroll = () => {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+  scrollFunction();
+};
+
+mybutton = document.getElementById("topButton");
+
+scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+function goToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+reveal();
